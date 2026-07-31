217d7db11386d538945424e2dbf6181309271db6
Franco Curotto <francocurotto@gmail.com>
2026-07-31 09:21:27 -0400

    start settings

--- app.gd
+++ app.gd
@@ -1,11 +1,5 @@
 extends Control
 
-# settings
-var n_reps_slow = 10
-var n_reps_fast = 10
-var t_reps_slow = 5.0
-var t_reps_fast = 1.0
-
 # variables
 var tween
 var size_rest
@@ -20,6 +14,7 @@ var size_squeeze
 @onready var vibrate_on_icon = preload("res://assets/icons/vibrate-on.svg")
 
 func _ready() -> void:
+	load_config_file()
 	var button_y = %ButtonMargin.custom_minimum_size.y
 	size_rest = Vector2(button_y, button_y)
 	size_squeeze = Vector2(button_y, button_y) / 1.5
@@ -41,22 +36,22 @@ func prepare_animation():
 
 func create_animation():
 	tween.tween_callback(on_start_slow_kegels)
-	for i in n_reps_slow:
-		kegel_animation(i, n_reps_slow, t_reps_slow)
+	for i in Globals.n_reps_slow:
+		kegel_animation(i, Globals.n_reps_slow, Globals.t_reps_slow)
 	tween.tween_callback(on_start_fast_kegels)
-	for i in n_reps_fast:
-		kegel_animation(i, n_reps_fast, t_reps_fast)
+	for i in Globals.n_reps_fast:
+		kegel_animation(i, Globals.n_reps_fast, Globals.t_reps_fast)
 	tween.tween_callback(on_kegels_finished)
 
 func on_start_slow_kegels():
 	%Speed.text = "Slow"
-	$Timer.wait_time = t_reps_slow
+	$Timer.wait_time = Globals.t_reps_slow
 	%OptionsRow.visible = true
 	$Timer.start()
 
 func on_start_fast_kegels():
 	%Speed.text = "Fast"
-	$Timer.wait_time = t_reps_fast
+	$Timer.wait_time = Globals.t_reps_fast
 
 func kegel_animation(count, n_reps, time):
 	tween.tween_callback(on_kegel_squeeze.bind(n_reps, count))
@@ -116,3 +111,13 @@ func _on_vibrate_button_toggled(toggled_on: bool) -> void:
 		%VibrateButton.icon = vibrate_on_icon
 	else:
 		%VibrateButton.icon = vibrate_off_icon
+
+func load_config_file():
+	var cfile = ConfigFile.new()
+	var err = cfile.load("res://settings.cfg")
+	if err != OK:
+		return
+	Globals.n_reps_slow = cfile.get_value("settings", "n_reps_slow")
+	Globals.n_reps_fast = cfile.get_value("settings", "n_reps_fast")
+	Globals.t_reps_slow = cfile.get_value("settings", "t_reps_slow")
+	Globals.t_reps_fast = cfile.get_value("settings", "t_reps_fast")
---
+++ globals.gd
@@ -0,0 +1,7 @@
+extends Node
+
+# configuration
+var n_reps_slow = 10
+var n_reps_fast = 10
+var t_reps_slow = 5.0
+var t_reps_fast = 1.0
